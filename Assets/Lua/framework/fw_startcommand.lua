6e9ecc9be311dd99ce0a0873c3a122f55776e854
lldoit <48618499@qq.com>
2016-07-05 18:18:47 +0800

    lua添加了middleclass

--- Assets/Lua/framework/fw_startcommand.lua
+++ Assets/Lua/framework/fw_startcommand.lua
@@ -1,11 +1,16 @@
-local network_manager = require "manager/network_manager"
-local ui_manager = require "ui/ui_manager"
-local fw_command = require "framework/fw_command"
-local ui_topbar = require "ui/ui_topbar"
-local ui_mainmenu_scene = require "ui/ui_mainmenu_scene"
+-- region *.lua
+-- Date
+-- 此文件由[BabeLua]插件自动生成
+local class = require("common/middleclass")
 
-local fw_startcommand = class(fw_command)
-function fw_startcommand:init()
+local network_manager = require("manager/network_manager")
+local ui_manager = require("ui/ui_manager")
+local fw_command = require("framework/fw_command")
+local ui_topbar = require("ui/ui_topbar")
+local ui_mainmenu_scene = require("ui/ui_mainmenu_scene")
+
+local fw_startcommand = class("fw_startcommand", fw_command)
+function fw_startcommand:initialize()
 end
 
 function fw_startcommand:execute(msg)
